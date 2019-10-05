b0eccb2c3657423392de7b591841852c4c1a44f7
wdelatour <wdelatour@utexas.edu>
2019-10-05 14:18:16 -0500

    Final Push for Submission

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -4,22 +4,28 @@ import re
 # set the path
 data_csv = os.path.join('paragraph.csv')
 
-#data_in = input('Enter in text. (Type "example" to use example.txt')
-#data_csv = os.path.join(data_in)
-#if data_in = example
-    #data_csv = os.path.join('paragraph.csv')
+data_in = input('Enter in text. (Type "example" to use example.txt) \n')
+data_csv = os.path.join(data_in)
+if data_in == "example":
+    data_csv = os.path.join('paragraph.csv')
  
 
 text = []
 word_count = 0
 #treat as csv
-with open(data_csv, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=" ")
-    for line in csvreader:
-        text.append(line)
+if data_in == "example":
+    with open(data_csv, newline="") as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=" ")
+        for line in csvreader:
+            text.append(line)
 
 full_string = ''
 sent_err = 0
+
+if data_in != "example":
+    full_string = data_in
+    text = data_in    
+
 abrev = ["Dr.", "Mr.", "Mrs.", "vs.", "Apt."]
 for line in text:
     for word in line:
@@ -32,18 +38,25 @@ for line in text:
 
 char_count = 0
 sent_count = 0
+space_count = 0
 for char in full_string:
     char_count = char_count + 1
-    if char == ".":
+    if char == '.' or char == '?':
         sent_count = sent_count + 1
+    if char == ' ':
+        space_count = space_count + 1
 sent_count = sent_count - sent_err
 #the_str = re.split("(?<=[.!?]) +", word)
-the_str  = re.split(r'[ \t\r\f\v]*\n[ \t\r\f\v]*\n[ \t\r\f\v]*', full_string)
-sent = full_string.find(". ")
+#the_str  = re.split(r'[ \t\r\f\v]*\n[ \t\r\f\v]*\n[ \t\r\f\v]*', full_string)
+#sent = full_string.find(". ")
+if data_in != "example":
+    full_string = data_in
+char_wo = char_count - word_count
 
-
-avg_letter = char_count / word_count
-avg_sent_len = word_count /sent_count
+avg_letter = char_wo / word_count
+avg_sent_len = 0
+if sent_count != 0:
+    avg_sent_len = word_count / sent_count
 print("--------------------")
 print("Full Text")
 print("--------------------")
@@ -53,5 +66,5 @@ print("Paragraph Analysis")
 print("-----------------")
 print(f"Approximate Word Count: {word_count}")
 print(f"Approximate Sentence Count: {sent_count}")
-print(f"Average Letter Count: {avg_letter}")
-print(f"Average Sentence Length: {avg_sent_len}")
+print(f"Average Letter Count: {round(float(avg_letter), 1)}")
+print(f"Average Sentence Length: {round(float(avg_sent_len), 1)}")
